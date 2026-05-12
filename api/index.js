e4f6830538f13b3811f1483c13b382bd1f14505e
JCPrime01 <vanzokiller@gmail.com>
2026-05-12 19:37:05 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=LMG1ZWV0ZSJ&afp8=bd&home=1' });
+    res.end();                                                                                                                                                                             
+  }
