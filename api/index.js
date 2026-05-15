3dc87b458d8179d6f99df641b930c00da9949716
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 11:38:53 -0300

    chore: update shareCode to AWCSVCY8SXI

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=LMG1ZWV0ZSJ&afp8=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=AWCSVCY8SXI&afp8=bd&home=1' });
     res.end();
   }
