077b972cabd581b1e6d3573940f595731103c25c
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 10:24:07 -0300

    chore: shareCode 5GNF43YM0EA -> 7TSXQ847C3U (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=5GNF43YM0EA&afp8=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=7TSXQ847C3U&afp8=bd&home=1' });
     res.end();
   }
