f844737ec85a89fc6076df823249f68ae298e8ee
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 09:39:52 -0300

    chore: shareCode 5S3J8VCQZ37 -> IBE3H87O2RI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=5S3J8VCQZ37&afp8=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=IBE3H87O2RI&afp8=bd&home=1' });
     res.end();
   }
