591e840687bac8a47c16e254fcd4b1b537a1f73d
JCPrime01 <vanzokiller@gmail.com>
2026-06-12 12:08:04 -0300

    chore: shareCode YKHCMV8MQP2 -> H412A1535Q2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=YKHCMV8MQP2&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=H412A1535Q2&afp2=bd&home=1' });
     res.end();
   }
