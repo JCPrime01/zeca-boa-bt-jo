d47fe405cc99a30e069e9ca3276ed5cbb734bcd7
JCPrime01 <vanzokiller@gmail.com>
2026-06-13 10:38:06 -0300

    chore: shareCode H412A1535Q2 -> 4MSIRBP5RD9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=H412A1535Q2&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=4MSIRBP5RD9&afp2=bd&home=1' });
     res.end();
   }
