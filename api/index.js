21fa52a4f96310c12c05347d0d7952bbefbbc55e
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 11:05:20 -0300

    chore: shareCode 6V929IZ80TR -> FCIK26H0QYQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=6V929IZ80TR&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=FCIK26H0QYQ&afp2=bd&home=1' });
     res.end();
   }
