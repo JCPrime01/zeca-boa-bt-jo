dcec4a272cff52fdc3cca8e80de4ebea8eeaf757
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 09:32:18 -0300

    chore: shareCode RLUQYN2LB2U -> OR504KU6ZOS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=RLUQYN2LB2U&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=OR504KU6ZOS&afp2=bd&home=1' });
     res.end();
   }
