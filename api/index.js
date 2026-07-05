895e7de14f831597ad5b5c83193dffcb75969903
JCPrime01 <vanzokiller@gmail.com>
2026-07-05 11:58:28 -0300

    chore: shareCode HFC9HC5IDFR -> 45KKBJAGHBV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=HFC9HC5IDFR&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=45KKBJAGHBV&afp2=bd&home=1' });
     res.end();
   }
