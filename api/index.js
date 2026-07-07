cb135cdc670d981bddeb838b70cc2485895e5a6f
JCPrime01 <vanzokiller@gmail.com>
2026-07-07 10:15:47 -0300

    chore: shareCode 45KKBJAGHBV -> 2Y11X9AEDK6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=45KKBJAGHBV&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=2Y11X9AEDK6&afp2=bd&home=1' });
     res.end();
   }
